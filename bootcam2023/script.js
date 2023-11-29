384bb17646552837c8198e868d0864a7d3b31450
thomasirgis <152234876+thomasirgis@users.noreply.github.com>
2023-11-29 21:06:53 +0700

    Thomas Irgi Sampurno

---
+++ bootcam2023/script.js
@@ -0,0 +1,8 @@
+
+function ubahteks(){
+    let nama = "Thomas Irgi Sampurno";
+    document.getElementById("judul").innerHTML = "Universitas Ma'arif Nahdlatul Ulama Kebumen"
+    document.getElementById("paragraf").innerHTML = "Teks diubah oleh JavaScript"
+    document.getElementById("nama").innerHTML = nama
+}
+
